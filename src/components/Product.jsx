48b3da148977dcb08385b65c93cc65b90866efe6
Gabriel07S <gf275964@gmail.com>
2025-07-14 22:57:16 +0000

    Desafio5_Etapa2V1.0

---
+++ src/components/Product.jsx
@@ -0,0 +1,14 @@
+import styles from "./Product.module.css";
+
+export function Product({ thumbnail, title, description, price }) {
+  return (
+      <div className={styles.card}>
+ <img src={thumbnail} alt={title} className={styles.image} />
+ <h2>{title}</h2>
+<p>{description}</p>
+ <p>R$ {price}</p>
+<button>Adicionar ao carrinho</button>
+</div>
+);
+}
+                                          
